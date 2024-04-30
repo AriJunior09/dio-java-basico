4232b86103ffc162bc7ced4dc13d55f0278bff12
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-04-30 20:35:02 -0300

    refactor: update Aula02

--- primeiro-projeto/src/sintaxe/Aula02.java
+++ primeiro-projeto/src/sintaxe/Aula02.java
@@ -1,8 +1,8 @@
 public class Aula02{
     public static void main(String[] args){
-      int numero = 5;
-      System.out.println(--numero);
-      System.out.println(numero);
+      boolean variavel = true;
+      System.out.println(!variavel);
+      System.out.println(variavel);
 
     }
 }
