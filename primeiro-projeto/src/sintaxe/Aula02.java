a843fad63664fa1c6babf113cae8646f4f5f5b5c
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-04-30 20:12:31 -0300

    add Aula02

---
+++ primeiro-projeto/src/sintaxe/Aula02.java
@@ -0,0 +1,10 @@
+public class Aula02{
+    public static void main(String[] args){
+        int numero = 5;
+        numero = - numero;
+        System.out.println(numero);
+        numero = + numero;
+        System.out.println(numero);
+
+    }
+}
