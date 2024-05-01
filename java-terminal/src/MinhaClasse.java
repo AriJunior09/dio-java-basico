cc3a606e2df955d50ef0b67476ed2f31ca95b192
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-01 10:45:05 -0300

    feat: update minhaclasse

--- java-terminal/src/MinhaClasse.java
+++ java-terminal/src/MinhaClasse.java
@@ -1,7 +1,7 @@
 public class MinhaClasse {
     public static void main(String[] args){
-        System.out.println("Oi, turma fui executado pelo Terminal");
+        System.out.println("Teste de Terminal");
         
-    
     }
+    
 }
