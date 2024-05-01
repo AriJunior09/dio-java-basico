0eac22ae7a87605d2cced1822783d6d89c187598
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-04-30 23:49:45 -0300

    feat: update final de palavrasReservadas

--- primeiro-projeto/src/palavrasReservadas/Aula01.java
+++ primeiro-projeto/src/palavrasReservadas/Aula01.java
@@ -4,8 +4,9 @@ public class Aula01 {
     public static void main(String[] args) {
         
     Aula02 palavras = new Aula02();
-    System.out.println(palavras.publico);
-
+    
+    System.out.println(palavras.publica);
+    System.out.println(palavras.protegida);
     
     }
     
--- primeiro-projeto/src/palavrasReservadas/Aula02.java
+++ primeiro-projeto/src/palavrasReservadas/Aula02.java
@@ -1,31 +1,12 @@
 package palavrasReservadas;
 
 public class Aula02 {
-    public String publico = "Atributo e método Para todo mundo!";
-    private String privada = "A Nivel de classe";
-    protected String protegida = "Só para herança!";
+    public String publica = "PUBLIC - Atributo e método para todo mundo!";
+    private String privada = "PRIVADA - Só para nivel de classe";
+    protected String protegida = "PROTEGIDA - Só para herança!";
     public char[] getPublico;
     
-
-    public String getPublico() {
-        return publico;
-    }
-    public void setPublico(String publico) {
-        this.publico = publico;
-    }
-    public String getPrivada() {
-        return privada;
-    }
-    public void setPrivada(String privada) {
-        this.privada = privada;
-    }
-    public String getProtegida() {
-        return protegida;
-    }
-    public void setProtegida(String protegida) {
-        this.protegida = protegida;
-    }
-    
+  
     public String toString(String nome){
         return nome;
 
