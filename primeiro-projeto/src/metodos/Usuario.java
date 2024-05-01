e9353f37829400df5d03b67097c99da2b53f3646
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-04-30 23:04:05 -0300

    feat: update final exercicio

--- primeiro-projeto/src/metodos/Usuario.java
+++ primeiro-projeto/src/metodos/Usuario.java
@@ -13,6 +13,10 @@ public class Usuario {
         smartTv.aumentarVolume();
         System.out.println("Volume Atual:  " +smartTv.volume);
 
+        System.out.println("Canal Atual:  " + smartTv.canal);
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Atual:  " + smartTv.canal);
+
         System.out.println("Tv Ligada: " + smartTv.ligada);
         System.out.println("Canal Atual:  " + smartTv.canal);
         System.out.println("Volume Atual:  " +smartTv.volume);
