02dc02ebd5427f6a35af24a4f9a532f1d5ff47d9
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-01 12:01:57 -0300

    feat: update pasta ContaBanco

--- ContaBanco/src/App.java
+++ ContaBanco/src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args){
+        ContaTerminal conta = new ContaTerminal();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o número da conta: ");
+         int numero = sc.nextInt();
+
+         System.out.print("Digite o nome da agência: ");
+         String agencia = sc.next();
+
+         System.out.print("Digite o nome do cliente: ");
+         String nomeCliente = sc.next();
+
+
+
+
+
+    } 
+    
 }
---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,40 @@
+public class ContaTerminal {
+    public int numero;
+    public String agencia;
+    public String nomeCliente;
+    public double saldo;
+    
+
+    public ContaTerminal(){
+
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+    
+}
