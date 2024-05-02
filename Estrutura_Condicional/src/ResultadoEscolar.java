127395d047ff5611006bde631c0a8e1c58d6cb30
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-02 20:49:22 -0300

    feat: update resultado

--- Estrutura_Condicional/src/ResultadoEscolar.java
+++ Estrutura_Condicional/src/ResultadoEscolar.java
@@ -1,24 +1,25 @@
-import java.util.Scanner;
-
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         
-        System.out.print("Digite a primeira nota: ");
-        int nota1 = sc.nextInt();
-        System.out.print("Digite a segunda nota: ");
-        int nota2 = sc.nextInt();
-        double media = (nota1 + nota2) / 2;
-        System.out.printf("A sua média é: %.2f%n", media);
+        int nota = 7;
 
-        if(media >= 7)
+/*         if(nota >= 7){
         System.out.println("Aprovado");
+        }
 
-        else
+        else if(nota < 7 && nota >= 5){
+        System.out.println("Recuperação!");
+        }
+
+        else{
         System.out.println("Reprovado!");
-
-        sc.close();
+        } */
         
+        /* String resultado = nota >= 7 ? "Aprovado" : "Reprovado"; */
+
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+
+        System.out.println(resultado);
     }
     
 }
