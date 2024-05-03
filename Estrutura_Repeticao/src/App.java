d2821944b3afad8f09d7c7cd203a1681356eeb56
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-02 22:15:46 -0300

    feat: update EstruturaRepeticao

--- Estrutura_Repeticao/src/App.java
+++ Estrutura_Repeticao/src/App.java
@@ -1,9 +1,9 @@
 public class App {
     public static void main(String[] args) {
 
-        for (int i = 0; i <= 20; i++) {
+        for (int i = 0; i <= 10; i++) {
             System.out.println("Valor de i: " + i);
         }
-        System.out.println();
+        System.out.println("Fim do programa!");
     }
 }
