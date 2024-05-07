c842962040774ad828f3d6be2d7af273ff8bdab2
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-06 21:51:56 -0300

    feat: update contador

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -21,7 +21,7 @@ public class Contador {
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
         // Validar se parametroUm é MAIOR que parametroDois e lançar a exceção
         if (parametroUm > parametroDois) {
-            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+            throw new ParametrosInvalidosException("mensagem");
         }
 
         int contagem = parametroDois - parametroUm; 
