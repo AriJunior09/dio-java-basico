1c5be467fd5f25e48efdee1f5034f161a20549d8
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-09 21:06:19 -0300

    feat: update Aula02

--- primeiro-projeto/src/sintaxe/Aula02.java
+++ primeiro-projeto/src/sintaxe/Aula02.java
@@ -1,10 +1,36 @@
 package sintaxe;
+import java.util.Scanner;
 
 public class Aula02{
     public static void main(String[] args){
-      boolean variavel = true;
-      System.out.println(!variavel);
-      System.out.println(variavel);
+      /*int idadeAri = 32;
+      int idadeRute = 30;
+      verificarMaisVelho(idadeAri, idadeRute);*/
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Digite uma palavra: ");
+      String palavra = sc.nextLine();
 
+      contarLetras(palavra);
+
+
+      sc.close();
     }
+
+    static int contarLetras(String palavra){
+      System.out.println("A palavra tem: " + palavra.length() + " letras");
+      return 0;
+    }
+
+    /*static void verificarMaisVelho(int idade1, int idade2){
+      if(idade1 > idade2){
+        System.out.printf(" %d é mais velho!", idade1);
+      }else if(idade1 == idade2){
+        System.out.printf("Ambas tem a mesda idade! ");
+      }else{
+        System.out.printf(" %d é mais velho!", idade2);
+      }
+    }*/
+
+    
+
 }
